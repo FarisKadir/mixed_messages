cbf3d3229bc8c114cdcac6e0ae0529257260b55a
Faris Kadir <faris.kadir@gmail.com>
2023-11-22 14:02:47 -0500

    created variables and arrays to store message components

--- script.js
+++ script.js
@@ -4,17 +4,54 @@ What types of messages should be sent? Astrology
 
 What pieces of the messages can stay the same?
 
-{greeting} {message}
+Greeting - array of objects
+Verb - array of objects
+Action / Statement - array
 
-Greetings and messages should be separate arrays. There should be an array of messages for each greeting
+Person:
+    You
+    Your friend
+    Someone close to you
+    Your spouse
 
-Greetings:
-    You are
-    You are having 
-    You will
+Verbs:
+    are
+    will
+    should
 
-    You are - array1
-    you are having - array2
-    You will - array3
+Action / Statements:
+    are 
+        having a great day. Keep positive thoughts.
+        going to be excited this upcoming week. Try to contain your excitement.
+        having good luck! Try your hand at the lotto!
+    will
+        win the lotto this month!
+        have good luck this week!
+        start a new career. 
+    should
+        try something new.
+        try to stay positive even if things are not great.
+        learn something new.
+
 
 */
+
+const person = ['You', 'Someone close to you', 'An old acquaintance'];
+
+
+const verb_messages = [
+    {
+        verb: 'are',
+        messages: ['having a great day. Keep positive thoughts.', 'going to be excited this upcoming week. Try to contain your excitement', 'having good luck! Try your hand at the lotto!']
+    },
+    {
+        verb:   'will',
+        messages: ['win the lotto this month!', 'have good luck this week!', 'start a new career.']
+    },
+    {
+        verb:   'should',
+        messages: ['try something new.', 'try to stay positive even if things are not great.', 'learn something new.']
+    }
+];
+
+
